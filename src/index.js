899f42715152c70bbf029bc8c0c8477e9b38e0aa
Julio Andrade <julioasilva78@gmail.com>
2021-10-03 19:57:23 -0300

    Api Financeira

---
+++ src/index.js
@@ -0,0 +1,140 @@
+const express = require("express");
+const { v4: uuidv4 } = require("uuid")
+
+const app = express();
+
+//Para trabalhar com arquivos json
+app.use(express.json());
+
+const customers = [];
+
+
+//middleware para verificar se a conta existe
+function verifyIfExistsAccountCPF(request, response, next) {
+   const { cpf } = request.headers;
+
+   const customer = customers.find((customer) => customer.cpf === cpf);
+
+   if (!customer) {
+    return response.status(400).json({ error: "Customer NOT FOUND" });
+  }
+   
+  request.customer = customer;
+
+  return next();
+
+}
+
+function getBalance(statement) {
+  const balance = statement.reduce((acc, operation) => {
+     if (operation.type === "credit") {
+       return acc + operation.amount;
+    } else { return acc - operation.amount;
+
+    }
+  }, 0)
+   
+  return balance
+
+}
+
+app.post("/account", (request, response) => {
+  const { cpf, name }= request.body;
+
+  const customerAlreadyExists = customers.some((customer) => customer.cpf === cpf);
+
+  if(customerAlreadyExists) {
+      return response.status(400).json({error: "Customer already exists"});
+  }
+
+
+  customers.push({
+      cpf,
+      name, 
+      id: uuidv4(), 
+      statement: []
+   });
+
+   return response.status(201).send()
+});
+
+ //app.use(verifyIfExistsAccountCPF);
+
+app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
+    const { customer } = request;
+  
+    return response.json(customer.statement);
+
+});
+
+//Rota para fazer os depositos
+app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
+  const { description, amount} = request.body;
+
+  const { customer } = request;
+   
+  //Para detalhar a operação
+  const statementOperation = {
+    description,
+    amount,
+    created_at: new Date(),
+    type: "credit"
+  }
+   customer.statement.push(statementOperation)
+
+   return response.status(201).send();
+});
+
+//Rota para ver o extrato com a data
+app.get("/statement/date", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+  const { date } = request.query;
+
+  const dateFormat = new Date(date + " 00:00");
+   
+  //Filtrar a data e formatar para o horário
+  const statement = customer.statement.filter((statement) =>
+  statement.created_at.toDateString() === 
+  new Date(dateFormat).toDateString())
+
+  return response.json(statement);
+});
+
+//Atualizar usuario
+app.put("/account", verifyIfExistsAccountCPF, (request, response) => {
+    const { name } = request.body;
+    const { customer } = request;
+
+    customer.name = name;
+
+    return response.status(201).send();
+});
+
+//Rota para buscar as contas dos clientes
+app.get("/account", verifyIfExistsAccountCPF, (request, response) => {
+   const { customer } = request;
+
+   return response.json(customer);
+
+})
+
+app.delete("/account", verifyIfExistsAccountCPF, (request, response) =>{
+   const { customer } = request;
+
+   customers.splice(customer, 1);
+
+   return response.status(200).json(customers);
+
+})
+
+app.get('/balance', verifyIfExistsAccountCPF, (request, response) =>{
+
+   const { customer } = request;
+
+   const balance = getBalance(customer.statement);
+
+   return response.json(balance);
+
+})
+
+app.listen(3333);
